b075770a4c39c06138c0cf7c49fc89477c17cc11
Gabriel Molina <gabrielmolina149@gmail.com>
2023-06-05 15:20:10 -0300

    vista_alfa

--- vista.py
+++ vista.py
@@ -17,11 +17,12 @@ class Ventana:
             master=self.root,
             text=f"Version del programa: {modelo.__version__} \n INGRESAR LOG: "
         )
-        self.instruc_princ.pack()
+        self.instruc_princ.grid(row=0, columnspan=3, column=0, padx=10)
 
         def obtener_texto():
             texto = self.caja_mensaje.get("1.0", END)
             self.registro.entrada_log(texto)
+            aviso_lab["text"].set("--guardado--")
 
         # Entrada de mensaje
         self.caja_mensaje = Text(
@@ -29,11 +30,21 @@ class Ventana:
             height=10, 
             width=40
         )
-        self.caja_mensaje.pack()
+        self.caja_mensaje.grid(row=1, columnspan=3, column=0, padx=10)
 
-        boton = Button(
+        aviso_g = StringVar()
+        aviso_lab = Label(master=self.root, text=aviso_g)
+        aviso_lab.grid(row=2, column=1)
+
+        boton_g = Button(
             self.root, 
             text="Guardar registro", 
             command=obtener_texto)
-        boton.pack()
+        boton_g.grid(row=2, columnspan=3, column=0, sticky="w", padx=10)
+        
+        boton_l = Button(
+            self.root, 
+            text="Ver registro", 
+            command=obtener_texto)
+        boton_l.grid(row=2, columnspan=3, column=2, sticky="e", padx=10)
     
